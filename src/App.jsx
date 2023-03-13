435487ba7555522631ab9ebdfcf09faad14bd829
Shivam Pandey <sjtgshivam@gmail.com>
2023-03-13 16:14:27 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import "./App.css";
+import { Time } from "./components/Time";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const nowOrGreaterTime = (val, name, formProps) => {
+    let currentHour = new Date().toLocaleTimeString().split(":")[0];
+    let currentMinute = new Date().toLocaleTimeString().split(":")[1];
+    if (val == "") return;
+    if (name === "hour") {
+      if (currentHour < val) return;
+      if (currentHour == val) {
+        if (formProps.values.minute >= currentMinute) return;
+        formProps.form.mutators.setValue("minute", currentMinute);
+      } else {
+        formProps.form.mutators.setValue("hour", currentHour);
+        formProps.form.mutators.setValue("minute", currentMinute);
+      }
+    }
+    if (name === "minute") {
+      if (formProps.values.hour > currentHour) return;
+      if (val >= currentMinute) return;
+      formProps.form.mutators.setValue("minute", currentMinute);
+    }
+  };
+  const nowOrLessTime = (val, name, formProps) => {
+    let currentHour = new Date().toLocaleTimeString().split(":")[0];
+    let currentMinute = new Date().toLocaleTimeString().split(":")[1];
+    if (val == "") return;
+    if (name === "hour") {
+      if (currentHour > val) return;
+      if (currentHour == val) {
+        if (formProps.values.minute <= currentMinute) return;
+        formProps.form.mutators.setValue("minute", currentMinute);
+      } else {
+        formProps.form.mutators.setValue("hour", currentHour);
+        formProps.form.mutators.setValue("minute", currentMinute);
+      }
+    }
+    if (name === "minute") {
+      if (formProps.values.hour < currentHour) return;
+      if (val <= currentMinute) return;
+      formProps.form.mutators.setValue("minute", currentMinute);
+    }
+  };
+  return (
+    <div className="App">
+      <p>
+        This is to demonstrate how we can reuse same component with different
+        Logical needs by passing the working logic as prop.
+      </p>
+      <div>
+        Can input current of greater than current time:
+        <Time handleTimeLimitation={nowOrGreaterTime} />
+        Can input current of less than current time:
+        <Time handleTimeLimitation={nowOrLessTime} />
+        Can input any appropriate time:
+        <Time />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Time.jsx
@@ -0,0 +1,65 @@
+import { useCallback } from "react";
+import { Field, Form } from "react-final-form";
+import { debounce } from "throttle-debounce";
+
+const defaultTimeLimitation = (val, name, formProps) => {
+  if (
+    (name === "hour" && (val > 23 || val < 0) && val !== "") ||
+    (name === "minute" && (val > 59 || val < 0) && val !== "")
+  )
+    return;
+  formProps.form.mutators.setValue(name, val);
+};
+
+function Time({ handleTimeLimitation = () => {} }) {
+  const debouncedHandleTimeLimitation = useCallback(
+    debounce(800, handleTimeLimitation),
+    []
+  );
+  return (
+    <div>
+      Time is here:
+      <Form
+        onSubmit={() => {
+          console.log("submit");
+        }}
+        mutators={{
+          setValue: ([field, value], state, { changeValue }) => {
+            changeValue(state, field, () => value);
+          },
+        }}
+        render={(props) => (
+          <>
+            <div>
+              <Field
+                name="hour"
+                component="input"
+                type="number"
+                className="inp"
+                onInput={({ target: { value, name } }) =>
+                  defaultTimeLimitation(value, name, props)
+                }
+                onChange={({ target: { value, name } }) =>
+                  debouncedHandleTimeLimitation(value, name, props)
+                }
+              />
+              <Field
+                name="minute"
+                component="input"
+                type="number"
+                className="inp"
+                onInput={({ target: { value, name } }) =>
+                  defaultTimeLimitation(value, name, props)
+                }
+                onChange={({ target: { value, name } }) => {
+                  debouncedHandleTimeLimitation(value, name, props);
+                }}
+              />
+            </div>
+          </>
+        )}
+      />
+    </div>
+  );
+}
+export { Time };
